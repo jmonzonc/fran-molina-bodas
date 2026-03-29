8eb874002a3e3e0b4efc7e1460db2ed756bfb337
jmonzonc <jmonzoncarabe@protonmail.com>
2026-03-29 13:01:18 +0200

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,18 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  typescript: {
-    ignoreBuildErrors: true,
-  },
   images: {
-    unoptimized: true,
+    formats: ["image/avif", "image/webp"],
+    deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048],
+    imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
     remotePatterns: [
       {
-        protocol: 'https',
-        hostname: 'images.unsplash.com',
-      },
-      {
-        protocol: 'https',
-        hostname: 'assets.mixkit.co',
+        protocol: "https",
+        hostname: "clmmicwprzdhnkbeczoi.supabase.co",
+        pathname: "/storage/v1/object/**",
       },
     ],
   },

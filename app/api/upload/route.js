f3478e42facbac0c5485584381a36751613fcb92
jmonzonc <jmonzoncarabe@protonmail.com>
2026-04-03 17:48:15 +0200

    Create route.js

---
+++ app/api/upload/route.js
@@ -0,0 +1,23 @@
+import { PutObjectCommand } from "@aws-sdk/client-s3";
+import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
+import { s3 } from "@/lib/storage";
+import { randomUUID } from "crypto";
+
+export async function POST(request) {
+  const { filename, contentType, contentLength } = await request.json();
+
+  const ext = filename.split(".").pop();
+  const key = `uploads/${randomUUID()}.${ext}`;
+
+  const command = new PutObjectCommand({
+    Bucket: process.env.B2_BUCKET_NAME,
+    Key: key,
+    ContentType: contentType,
+    ContentLength: contentLength,
+  });
+
+  const signedUrl = await getSignedUrl(s3, command, { expiresIn: 900 });
+  const publicUrl = `${process.env.NEXT_PUBLIC_CDN_URL}/${key}`;
+
+  return Response.json({ signedUrl, publicUrl });
+}
